72c399c3e5394690c8ea301a1ec5d25862f62310
Mehdi Ali <mehdikhanofficial@gmail.com>
2024-05-23 16:14:52 +0500

    Updated

--- routes/listing.js
+++ routes/listing.js
@@ -4,7 +4,7 @@ const wrapAsync = require('../utils/wrapAsync.js')
 const { isLoggedIn, isOwner, validateListing } = require('../middleware.js')
 
 const multer = require('multer');
-const {storage} = require('../cloudconfig.js');
+const {storage} = require("../cloudConfig.js");
 const upload = multer({storage});
 
 
