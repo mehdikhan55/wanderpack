528e332513642ba8221f3ae1aee2eb83e9e34771
Mehdi Ali <mehdikhanofficial@gmail.com>
2024-05-23 15:18:06 +0500

    Updated

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const userRouter= require("./routes/users.js");
 
 main().then(() => console.log('Connected to DB...')).catch(err => console.log('Error:', err));
 async function main() {
-    await mongoose.connect(dbUrl)
+    await mongoose.connect(`${dbUrl}`)
 }
 
 
@@ -45,7 +45,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
+    mongoUrl: `${dbUrl}`,
     crypto:{
         secret: process.env.SECRET,
         touchAfter: 24 * 3600, //time period in seconds
